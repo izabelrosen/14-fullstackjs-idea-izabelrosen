c25057fbf8e4eb10ad2f218d633df2a2fc6e5819
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-09-11 12:06:04 +0200

    create a new user

--- server/models/User.js
+++ server/models/User.js
@@ -2,21 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    name: { type: String, maxlength: [100, 'Wooh, slow down buddy - too long name.']},
-    username: { type: String, maxlength: [50, 'Uhmm try a shorter username!']},
-    email: String,
-    password: { type: String, minlength: [4, 'Password must contain at least 8 characters']},
-    avatar: { type: String, default: ""},
-    chats: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Chat'
-
-    }]
+    name: String
 });
 
 UserSchema.pre('save', function(next) {
     var user = this;
+
+    if(!user) return next(error)
+    console.log(user)
     var roundsOfSalt = 5;
+    return next();
 })
 
 module.exports = mongoose.model('User', UserSchema);
