817bba0ed9f7330e6eef300607e48ae1f5313e16
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-09-21 14:52:52 +0200

    add model for message

---
+++ server/models/Message.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Describe a message
+var MessageSchema = new mongoose.Schema({
+    createdAt: { type: Date, default: Date.now},
+    users: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    text: String,
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
