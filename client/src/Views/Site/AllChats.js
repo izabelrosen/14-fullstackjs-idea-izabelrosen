cf666668b7002efe2f2e7c6b8eca4a0b393f0492
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-09-25 12:06:46 +0200

    wip: view for all chats

---
+++ client/src/Views/Site/AllChats.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AllChatsList } from '../../Components/Forms/AllChatsList';
+
+const AllChats = () => {
+    return (
+        <div className="AllChats">
+            <AllChatsList />
+        </div>
+    )
+}
+
+export default AllChats;
