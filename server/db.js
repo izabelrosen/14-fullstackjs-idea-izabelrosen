c712e3a842d4f9f82700b1dab4643b428355432e
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-09-25 12:15:32 +0200

    move db connection

--- server/db.js
+++ server/db.js
@@ -1,6 +1,4 @@
-//Connect to mongoDatabase
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://admin:password@ds261118.mlab.com:61118/prattle')
-// mongoose.connect('mongodb://admin:secret@ds261118.mlab.com:61118/prattle');
+// //Connect to mongoDatabase
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb://admin:password@ds145412.mlab.com:45412/prattle');
 
-// mongodb://<dbuser>:<dbpassword>@ds145412.mlab.com:45412/prattle
