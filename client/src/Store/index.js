66e61918b2ba1d322dba4b24cfce02370335aefc
Izabel Rosén <izabel.rosen@chasacademy.se>
2019-02-26 20:41:39 +0100

    add compose to be able to use redux devtools extension

--- client/src/Store/index.js
+++ client/src/Store/index.js
@@ -1,16 +1,20 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../Reducers';
 
 const initialState = {};
 
 const middleware = [thunk];
+/* eslint no-underscore-dangle: ["error", { "allow": ["__REDUX_DEVTOOLS_EXTENSION__"] }] */
 
 // spread because thunk is in an array
 const store = createStore(
   rootReducer,
   initialState,
-  applyMiddleware(...middleware),
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  ),
 );
 
 export default store;
