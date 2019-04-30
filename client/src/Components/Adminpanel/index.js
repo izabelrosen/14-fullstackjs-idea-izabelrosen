3bf90737f5d8719fc287cea2a54d3686110b00bb
Izabel Rosén <izabel.rosen@chasacademy.se>
2019-04-30 17:23:26 +0200

    add admin panel view

---
+++ client/src/Components/Adminpanel/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable class-methods-use-this */
+import React, { Component } from 'react';
+
+export default class AdminPanel extends Component {
+  render() {
+    return (
+      <div>
+        <h1>admin</h1>
+      </div>
+    );
+  }
+}
