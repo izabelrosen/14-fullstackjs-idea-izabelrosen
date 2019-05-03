95c415dfb2de2a59717ad37a622666f98d019720
Izabel Rosén <izabel.rosen@chasacademy.se>
2019-05-03 16:47:26 +0200

    admin dashboard 	- fetch users 	- map and render user emails 	- proptypes 	- connect to redux

--- client/src/Components/Adminpanel/index.js
+++ client/src/Components/Adminpanel/index.js
@@ -1,13 +1,30 @@
+/* eslint-disable react/no-deprecated */
 /* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import '../App';
+import { connect } from 'react-redux';
 import { List } from 'semantic-ui-react';
+import { fetchUsers } from '../../Actions/user';
 import './style.css';
 
-export default class AdminPanel extends Component {
+class AdminPanel extends Component {
+  componentWillMount = () => {
+    this.props.fetchUsers();
+  }
+
   render() {
+    const userItems = this.props.users.map(user => (
+      <div key={user.id}>
+        <h4>{user.email}</h4>
+      </div>
+    ));
     return (
       <div className="adminpanel">
         <h1>admin</h1>
+        <div>
+          <h4>{userItems}</h4>
+        </div>
 
   <List>
     <List.Item>
@@ -32,3 +49,12 @@ export default class AdminPanel extends Component {
     );
   }
 }
+AdminPanel.propTypes = {
+  fetchUser: PropTypes.func.isRequired,
+  users: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = state => ({
+  users: state.user.users,
+});
+export default connect(mapStateToProps, { fetchUsers })(AdminPanel);
