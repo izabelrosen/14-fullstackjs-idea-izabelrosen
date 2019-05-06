eb185e7b489ecf89b2665afd30f527ef5f506bef
Izabel Rosén <izabel.rosen@chasacademy.se>
2019-05-06 17:36:38 +0200

    scroll to view 	- add buttons on click 	- function that scrolls down on the page 	- does not want /# in browser address bar

--- client/src/Components/Portfolio/LandingPage.js
+++ client/src/Components/Portfolio/LandingPage.js
@@ -1,15 +1,33 @@
 /* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Button } from 'semantic-ui-react';
 import './style.css';
 
 export default class LandingPage extends Component {
+  constructor() {
+    super();
+    this.myProjects = this.myProjects.bind(this);
+    this.aboutMe = this.aboutMe.bind(this);
+  }
+
+  myProjects = () => {
+    const projects = document.getElementById('projects');
+    projects.scrollIntoView();
+  };
+
+  aboutMe = () => {
+    const aboutMe = document.getElementById('aboutMe');
+    aboutMe.scrollIntoView();
+  }
+
   render() {
     return (
       <section id="landingPage">
         <Header as='h2' icon textAlign='center'>
           <Header.Content>Izabel Rosen</Header.Content>
         </Header>
+        <Button onClick={() => this.myProjects()}>Projects</Button>
+        <Button onClick={() => this.aboutMe()}>About</Button>
       </section>
     );
   }
