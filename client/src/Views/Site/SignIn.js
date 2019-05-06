c0fc0a571d97ccf4dc8f9f6d14d3a0f419882425
Izabel Rosén <izabel.rosen@chasacademy.se>
2019-05-06 16:10:26 +0200

    fix eslint errors

--- client/src/Views/Site/SignIn.js
+++ client/src/Views/Site/SignIn.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import SignInForm from '../../Components/Forms/SignInForm';
 
-/*  eslint arrow-body-style: ["error", "always"]  */
-
-const SignIn = () => {
-  return (
-    <div className="SignIn">
-      <SignInForm />
-    </div>
-  );
-};
+const SignIn = () => (
+  <div className="SignIn">
+    <SignInForm />
+  </div>
+);
 
 export default SignIn;
--- client/src/Views/Site/SignUp.js
+++ client/src/Views/Site/SignUp.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import SignUpForm from '../../Components/Forms/SignUpForm';
 
-/*  eslint arrow-body-style: ["error", "always"]  */
-
-const SignUp = () => {
-  return (
-    <div className="SignUp">
-      <SignUpForm />
-    </div>
-  );
-};
+const SignUp = () => (
+  <div className="SignUp">
+    <SignUpForm />
+  </div>
+);
 
 export default SignUp;
